6837b9309bf7dc91a5238651bdd119b8094b2bbe
Ronald Guerra <ronaldg0304@gmail.com>
2026-06-14 14:41:20 -0500

    chore: initial project setup

---
+++ backend/src/test/java/com/demo/travel_expense_management/TravelExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.travel_expense_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelExpenseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
